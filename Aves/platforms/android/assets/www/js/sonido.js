497090d54a798cf973925950d65aafbd5849bee8
Ana Ma. Jalca <amjalca@espol.edu.ec>
2017-02-04 18:36:02 -0500

    Avance
    Sonido 100%

--- Aves/platforms/android/assets/www/js/sonido.js
+++ Aves/platforms/android/assets/www/js/sonido.js
@@ -1,185 +1,108 @@
 var app={
     
   inicio: function(){
-    this.iniciaBtnMHabtC();
-    this.iniciaBtnMHabtD();
-    this.iniciaBtnMHabtP();
-    this.iniciaBtnMHabtB();
-    this.iniciaBtnMHabtS();
-    this.iniciaBtnMHabtL();
+    this.iniciaBtnMSonido();
     this.iniciaFastClick();
   },
   
-  iniciaBtnMHabtC: function(){
-     var botonHome = document.querySelector('#imgHome');
-      var modal = document.getElementById('myModal');
+  iniciaBtnMSonido: function(){
+    var botonHome = document.querySelector('#imgHome');
+    var botonEvaluar = document.querySelector('#btnSon');
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("prueba");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
     botonHome.addEventListener('click',this.nxtMnHome,false);
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-  },
- nxtMnHome: function(){
-      location.href="menu.html";
-    },
-  ////////////////////////////////
-  iniciaBtnMHabtD: function(){
-      var modal = document.getElementById('myModal2');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("prueba2");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close2")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+    botonEvaluar.addEventListener('click',this.btnEvaluarS,false);
+    
   },
 
-   ////////////////////////////////
-  iniciaBtnMHabtP: function(){
-      var modal = document.getElementById('myModal3');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("prueba3");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close3")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-  },
-   ////////////////////////////////
-  iniciaBtnMHabtB: function(){
-      var modal = document.getElementById('myModal4');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("prueba4");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close4")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+  nxtMnHome: function(){
+    location.href="menu.html";
   },
 
-   ////////////////////////////////
-  iniciaBtnMHabtS: function(){
-      var modal = document.getElementById('myModal5');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("prueba5");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close5")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+  btnEvaluarS: function(){
+    var avesSonido = document.getElementsByName('ave');
+    var txt = "";
+    var i;
+    for (i = 0; i < avesSonido.length; i++) {
+        if (avesSonido[i].checked) {
+            txt = txt + avesSonido[i].value;
         }
     }
+    if(txt=="Cacique Lomiamarillo"){
+        //var botonHome = document.querySelector('#imgHome');
+      // Get the modal
+      var modal = document.getElementById('myModal');
+      var im = document.getElementById('imgSon').src='resource/drawable/cacique_lomiamarillo.jpg';
+      var res = document.getElementById('resES').innerHTML="Correcto";
+      document.getElementById('resES').style='display: table;margin: 0 auto;';
+
+      // Get the button that opens the modal
+      //var btn = document.getElementById("btnMnEBclr");
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+
+      //botonHome.addEventListener('click',this.nxtMnHome,false);
+
+      // When the user clicks the button, open the modal 
+      //btn.onclick = function() {
+          modal.style.display = "block";
+
+      //}
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+          //modal.style.display = "none";
+          location.href="sonido.html";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+          if (event.target == modal) {
+              //modal.style.display = "none";
+              location.href="sonido.html";
+          }
+      }
+		}
+		else{
+			// Get the modal
+      var modal = document.getElementById('myModal');
+      var im = document.getElementById('imgSon').src='resource/drawable/incognito1.png';
+      var res = document.getElementById('resES').innerHTML=" ¡Vuelva a intentarlo!";
+      document.getElementById('resES').style='color:#E74C3C;display: table;margin: 0 auto;';
+
+      //set checked to false
+      var chOp = document.getElementsByName('ave');
+      for (i = 0; i < chOp.length; i++) {
+            avesSonido[i].checked = false;
+    }
+      // Get the button that opens the modal
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+
+      // When the user clicks the button, open the modal 
+      //btn.onclick = function() {
+          modal.style.display = "block";
+
+      //}
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+          modal.style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+          if (event.target == modal) {
+              modal.style.display = "none";
+          }
+      }		
+    }
+       
   },
 
-   ////////////////////////////////
-  iniciaBtnMHabtL: function(){
-      var modal = document.getElementById('myModal6');
+  
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("prueba6");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close6")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-  },
- 
   iniciaFastClick: function () {
     FastClick.attach(document.body);
   },
